87a3ac99bc0e9322f1ea0a77c3dcf8c3c453aa95
Pranjal Madan <100277894+Pmadan007@users.noreply.github.com>
2025-08-01 00:09:26 +0530

    Create createRoom.js

---
+++ netlify/functions/createRoom.js
@@ -0,0 +1,27 @@
+const fetch = require("node-fetch");
+
+exports.handler = async function (event) {
+  const { room_name } = JSON.parse(event.body || "{}");
+
+  const app_secret = "e5LfPS3lwoZRq76gt5QVKh6FZOpRx6me1oti17HiulXtz-pEILp3ARb5XD3jze71YTo6TCkYVmndW7FYXhoJ68-9YKAJGWoAMdk_8itncFFYpuxYCh3ZvfJXXXCOkHIT2wx-CoYsLZtSzmNvIfHqeSSMNWxWsUv2hDQdzu2OXSw=";
+  const template_id = "6481f99f2208a6e8d31cbb8b"; // replace if needed
+
+  const response = await fetch("https://api.100ms.live/v2/rooms", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${app_secret}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: room_name,
+      description: "Raksha Bandhan Room",
+      template_id: template_id,
+    }),
+  });
+
+  const data = await response.json();
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
+};
