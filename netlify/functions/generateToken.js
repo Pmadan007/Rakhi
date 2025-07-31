9818b62ee518d9539dd90701f6b046462814e192
Pranjal Madan <100277894+Pmadan007@users.noreply.github.com>
2025-08-01 00:05:34 +0530

    Update generateToken.js

--- netlify/functions/generateToken.js
+++ netlify/functions/generateToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 exports.handler = async function(event, context) {
-  const { user_id = "user", role = "host", room_id = "688ba850a48ca61c46476dcc" } = JSON.parse(event.body || "{}");
+  const { user_id = "user", role = "sister", room_id = "688ba850a48ca61c46476dcc" } = JSON.parse(event.body || "{}");
 
   const app_access_key = "688ba7bbbd0dab5f9a013465";
   const app_secret = "e5LfPS3lwoZRq76gt5QVKh6FZOpRx6me1oti17HiulXtz-pEILp3ARb5XD3jze71YTo6TCkYVmndW7FYXhoJ68-9YKAJGWoAMdk_8itncFFYpuxYCh3ZvfJXXXCOkHIT2wx-CoYsLZtSzmNvIfHqeSSMNWxWsUv2hDQdzu2OXSw=";
@@ -14,7 +14,7 @@ exports.handler = async function(event, context) {
     exp: Math.floor(Date.now() / 1000) + 24 * 60 * 60,
     room_id,
     user_id,
-    role
+    role,  // now defaults to "sister"
   };
 
   const token = jwt.sign(payload, app_secret);
