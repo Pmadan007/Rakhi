03031352a3766553e9cb294e427a4b257c4181f9
Pranjal Madan <100277894+Pmadan007@users.noreply.github.com>
2025-08-01 02:00:24 +0530

    Update generateToken.js

--- netlify/functions/generateToken.js
+++ netlify/functions/generateToken.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 exports.handler = async function(event) {
   const { room_id, role, user_name } = JSON.parse(event.body);
 
-  const managementToken = "YOUR_MANAGEMENT_TOKEN"; // Replace with actual
+  const managementToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3NTM5OTM4MDIsImV4cCI6MTc1NTIwMzQwMiwianRpIjoiMDdiYzg0YTYtN2ZkZi00MjFlLWE2NTctYjYzYmI0OTVmMDRkIiwidHlwZSI6Im1hbmFnZW1lbnQiLCJ2ZXJzaW9uIjoyLCJuYmYiOjE3NTM5OTM4MDIsImFjY2Vzc19rZXkiOiI2ODhiYTdiYmJkMGRhYjVmOWEwMTM0NjUifQ.SPvPZz7GriTQqxAjZ-FXE--P0E1QHLFND6OBl6SMx1w"; // Replace with actual
   const appId = "YOUR_APP_ID"; // Optional, for logs
 
   try {
